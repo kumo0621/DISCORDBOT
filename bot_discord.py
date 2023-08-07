06a05e5efa65a01c6de8055ab4e53f1ecb68cf90
kumo0621 <kumokumokumo0621@gmail.com>
2023-08-08 03:03:07 +0900

    へんこう

--- bot_discord.py
+++ bot_discord.py
@@ -1,111 +1,51 @@
-import discord
 import os
-from discord import Intents
-import psycopg2
+import socket
+import discord
+from discord.ext import commands
+import threading
 
-# PostgreSQLに接続するための情報
-host = 'localhost'
-port = 38244
-database = 'postgres'
-user = 'test'
-password = "test"
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 token = os.environ["DISCORD_TOKEN"]
+HOST = '0.0.0.0'
+PORT = 38244
 
-intents = Intents.default()
-intents.typing = False  # タイピングイベントを無効にする
-intents.presences = False  # プレゼンスイベントを無効にする
-intents.message_content = True
-intents.messages = True
+def bot_start():
+    bot.run(token)
 
-client = discord.Client(intents=intents)
+def socket_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print('サーバが起動しました。接続を待機しています。by nana')
+        while True:
+            conn, addr = s.accept()
+            print('Javaクライアントが接続しました。アドレス:', addr)
+            messages = [] # 新しくリストを定義する
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('Javaからのメッセージ:', data.decode('UTF-8'))
+                user_input = data.decode('UTF-8')
+                send_discord_message(user_input)
+            conn.close()
+            print('Javaクライアントとの接続を閉じました。')
 
-#データベースに接続
-def postgresql_handler(host, port, database, user, password, name, member_value):
-    conn = psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=user,
-        password=password
-    )
-    cur = conn.cursor()
-
-    def execute_query(query, *args):
-        cur.execute(query, args)
-        conn.commit()
-
-    def close_connection():
-        cur.close()
-        conn.close()
-        
-    #データベース削除
-    #query = "DROP TABLE IF EXISTS achievement_table"
-    #execute_query(query)
-
-    # PostgreSQLに接続して処理を実行する例
-    execute_query('''
-        CREATE TABLE IF NOT EXISTS achievement_table (
-            number SERIAL PRIMARY KEY,
-            id BIGINT,
-            name VARCHAR(255),
-            set VARCHAR(255)
-        )
-    ''')
-#データベースに入力する値
-    query = "INSERT INTO achievement_table (id, name, set) VALUES (%s, %s, %s)"
-    set = "初めて一週間"
-    args = (member_value, name, set)
-    execute_query(query, *args)
-    conn.commit()
-    execute_query("SELECT * FROM achievement_table")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    close_connection()
-
-
-#起動メッセージ
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-    # メッセージを送信するチャンネルを指定する
-    target_channel = await client.fetch_channel("1126060676989849712")  # 特定のチャンネルのID（CHANNEL_ID）を指定する
+def send_discord_message(message):
+    target_channel_id = 1126060676989849712
+    target_channel = bot.get_channel(target_channel_id)
 
     if target_channel is not None:
-        await target_channel.send('ボットが起動しました！')  # メッセージを送信する
+        bot.loop.create_task(target_channel.send(message))
 
-#リアクションの反応に対してロールを付与する
-@client.event
-async def on_raw_reaction_add(payload):
-    #if payload.message_id == "1126141547776311326":
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-        member = await guild.fetch_member(payload.user_id)
-        role = discord.utils.get(guild.roles, id=1126173114800279613)
-        if member is not None:
-            await member.add_roles(role)
-            name = "初心者"
-            member_value = payload.user_id
-            postgresql_handler(host, port, database, user, password, name, member_value)
-        else: print("error")
-    #else: print("erro2r")
+bot_thread = threading.Thread(target=bot_start)
+socket_thread = threading.Thread(target=socket_server)
 
-#サーバーに参加したときの初めての実績付与
-@client.event
-async def on_member_join(member):
-    print("join")
-    guild_id = member.guild.id
-    guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-    role = discord.utils.get(guild.roles, id=1126453426931388447)
-    if role is not None:
-        #await member.add_roles(role)
-        name = "初めまして"
-        member_value = member.id
-        postgresql_handler(host, port, database, user, password, name, member_value)
-    else:
-        print("Error: Role not found")
-#else: print("erro2r")
-client.run(token)
+bot_thread.start()
+socket_thread.start()
+
+bot_thread.join()
+socket_thread.join()
